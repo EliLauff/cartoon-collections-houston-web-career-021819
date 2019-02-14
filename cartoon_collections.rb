60e6303d137e32f0e428e2bba18c2cef1b7712f5
Eli Lauffenburger <eli.lauffenburger@gmail.com>
2019-02-14 21:29:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,5 @@ end
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
   ingredients.find do |item|
-    cheese_types.include?
+    cheese_types.include?(item)
 end
